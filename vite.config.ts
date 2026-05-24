c08bc9e7c0369583e2587052c0f67a26c7fe9f13
magia-auto-detail <magiaautodetailllc@gmail.com>
2026-05-23 21:57:03 -0700

    Fix formatting and add missing newline at end of file

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
+
 export default defineConfig({
   server: {
     port: 3000,
