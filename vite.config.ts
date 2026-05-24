28ae53502dd7245959475e50e2b694dc88740ded
magia-auto-detail <magiaautodetailllc@gmail.com>
2026-05-23 21:55:43 -0700

    Remove unnecessary line in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
-
 export default defineConfig({
   server: {
     port: 3000,
